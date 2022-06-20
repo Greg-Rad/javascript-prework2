8b87d007f5aa311d4101462aacfee9b3ead6f9d6
Grzegorz Radziszewski <greg.rad@yahoo.com>
2022-06-20 17:51:51 +0200

    Add script.js game-functions

---
+++ js/script.js
@@ -0,0 +1,6 @@
+var computerMove;
+computerMove = 'kamień';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+var playerMove;
+computerMove = 'papier';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nozyce, to wygrywasz!');
