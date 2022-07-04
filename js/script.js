ac49bd9b5a8cebfc40406c15df02ab4672b09eee
Grzegorz Radziszewski <greg.rad@yahoo.com>
2022-07-04 13:51:44 +0200

    Add computerMove

--- js/script.js
+++ js/script.js
@@ -1,15 +1,34 @@
 var computerMove;
 computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-var playerMove;
+
+computerMove = 'nożyce';
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to kamien, to wygrywasz!');
+
 computerMove = 'papier';
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nozyce, to wygrywasz!');
-var randomNumber;
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+var computerMove, randomNumber;
 randomNumber = Math.floor(Math.random() * 3 + 1);
-printMessage('Wylosowana liczba to: ' + randomNumber);
-var randomNumber;
-randomNumber = Math.floor(Math.random() * 8 + 12);
-printMessage('Wylosowana liczba to: ' + randomNumber);
+console.log('Wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+if (randomNumber == '2') {
+  computerMove = 'papier';
+} else {
+  computerMove = 'nieznany ruch';
+}
+if (randomNumber == '3') {
+  computerMove = 'nożyce';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+//var randomNumber;
+//randomNumber = Math.floor(Math.random() * 8 + 12);
+//printMessage('Wylosowana liczba to: ' + randomNumber);
 var playerMove, playerInput;
 playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 console.log('Wpisana odpowiedź to: ' + playerInput);
